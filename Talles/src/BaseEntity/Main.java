539574d1f0165f22d748cc516b5b024e2334c872
Icarooandrade11 <icarodeasantos@gmail.com>
2024-11-04 10:40:48 -0300

    Add files via upload

---
+++ Talles/src/BaseEntity/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(1, BaseEntity.STATUS_ATIVO, "Ícaro", "icaro@exemplo.com");
+        Pedido pedido = new Pedido(101, BaseEntity.STATUS_ATIVO, cliente, 250.75);
+
+        pedido.exibirDetalhes();
+    }
+}
